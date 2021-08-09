4c720f1e9ac07f4b7d1ad9d7f4eddff25aa0d07c
vidushidhakar <66857766+vidushidhakar@users.noreply.github.com>
2021-08-09 11:10:55 +0530

    Update MulesoftAssessment.py

--- MulesoftAssessment.py
+++ MulesoftAssessment.py
@@ -1,14 +1,11 @@
 import sqlite3
 
-#database connnection
 con=sqlite3.connect('moviesdatabase.db')
 cursor=con.cursor()
 
-#create table query
-query_command=""" CREATE TABLE IF NOT EXISTS Movies(movie_name TEXT PRIMARY KEY,lead_actor TEXT,actress TEXT,year_of_release INTEGER,director_name TEXT) """
+query_command=""" CREATE TABLE IF NOT EXISTS Movies(name TEXT PRIMARY KEY,actor TEXT,actress TEXT,year INTEGER,director TEXT) """
 cursor.execute(query_command)
 
-#insert into query
 cursor.execute(" INSERT INTO Movies VALUES ('Om Shanti Om','Shah Rukh Khan','Deepika Padukone',2007,'Farah Khan')")
 cursor.execute(" INSERT INTO Movies VALUES ('The Sky Is Pink','Farhan Akhtar','Priyanka Chopra Jonas',2019,'Shonali Bose')")
 cursor.execute(" INSERT INTO Movies VALUES ('3 Idiots','Aamir Khan','Kareena Kapoor',2009,'	Rajkumar Hirani')")
@@ -18,12 +15,10 @@ cursor.execute(" INSERT INTO Movies VALUES ('Joker','Joaquin Phoenix','Zazie Bee
 cursor.execute(" INSERT INTO Movies VALUES ('Inception','Leonardo DiCaprio','Elliot Page',2010,'Christopher Nolan')")
 cursor.execute(" INSERT INTO Movies VALUES ('Five Feet Apart','Cole Sprouse','Haley Lu Richardson',2019,'Justin Baldoni')")
 
-#select query
 cursor.execute("SELECT * FROM Movies")
 r1=cursor.fetchall()
-cursor.execute("SELECT * FROM Movies WHERE lead_actor='Cole Sprouse' ")
+cursor.execute("SELECT * FROM Movies WHERE actor='Cole Sprouse' ")
 r2=cursor.fetchall()
 
-#printing the results
 print(r1)
 print(r2)
